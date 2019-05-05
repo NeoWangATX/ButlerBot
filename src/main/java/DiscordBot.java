83fe6798e8c07c98131430a481d091e2c0fd91c4
Neo Wang <neowangatx@gmail.com>
2019-05-05 09:24:59 -0500

    allows users to talk through the bot

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,5 +1,7 @@
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+import java.util.Scanner;
+
 public class DiscordBot {
 
     public DiscordBot()
@@ -11,12 +13,49 @@ public class DiscordBot {
     {
         switch(command)
         {
-            case "credits":
+            case "credits": //credits
                 this.sendMessage(event, BotCommands.credits());
                 break;
-            case "paperclip":
+            case "paperclip": //paperclip
                 this.sendMessage(event, BotCommands.paperclip());
                 break;
+        }
+
+        //more complicated commands
+
+        if(event.getMessage().mentionsEveryone())
+        {
+            System.out.println("Did not respond because user pinged @everyone");
+        }
+        else if(command.contains("paperclip") && !(((event.getMessage().getContentRaw().charAt(11)+"").equals("<"))&&(event.getMessage().getContentRaw().endsWith(">"))))
+        {
+            System.out.println("Did not respond because user did not tag anyone");
+            this.sendMessage(event,"Proper usage: %paperclip <user>");
+        }
+        else if(command.substring(0,9).equals("paperclip") && command.length() > 10) //paperclip <user>
+        {
+            this.sendMessage(event, command.substring(10) + " " + BotCommands.paperclip());
+        }
+        else if(command.contains("TALK_THROUGH_BOT"))
+        {
+            System.out.println("Author : " + event.getMessage().getAuthor().getIdLong());
+            if(BotConfiguration.BOT_DEVELOPERS.contains(event.getMessage().getAuthor().getIdLong()) && event.getMessage().getContentRaw().contains("TALK_THROUGH_BOT"))
+            {
+                System.out.println("AUTHORIZED " +  event.getMessage().getAuthor().getName());
+                System.out.print("Send: ");
+                String message;
+                Scanner keyboard = new Scanner(System.in);
+
+                do {
+
+                    message = keyboard.nextLine();
+
+                    if(!message.isEmpty())
+                    {
+                        event.getChannel().sendMessage(message).queue();
+                    }
+                } while(!message.equals("STOP TYPING"));
+            }
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,39 @@
 import net.dv8tion.jda.bot.*;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.Scanner;
 
 public class Main extends ListenerAdapter{
-    private DiscordBot bot = new DiscordBot();;
+    private DiscordBot bot = new DiscordBot();
     public static void main(String[] args) throws LoginException
     {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         builder.setToken(BotConfiguration.BOT_TOKEN);
         builder.addEventListener(new Main());
         builder.buildAsync();
+    }
+
+    @Override
+    public void onGuildJoin(GuildJoinEvent event)
+    {
+        try
+        {
+            System.out.println("New server has added ButlerBot: " + event.getGuild().getInvites());
+        } catch (Exception e)
+        {
+            System.out.println("Bot is misssing permissions on " + event.getGuild().getName());
+        }
+
+        if(event.getGuild().getDefaultChannel().canTalk())
+        {
+            event.getGuild().getDefaultChannel().sendMessage("Thank you for adding ButlerBot. If you would like to donate to this bot, please dm tellem#7092").queue();
+        }
     }
 
     @Override
@@ -25,7 +45,8 @@ public class Main extends ListenerAdapter{
             return;
         }
 
-        System.out.println("Message from " +
+        System.out.println("Channel: #" +
+                event.getChannel().getName() + ", " +
                 event.getAuthor().getName() + ": " +
                 event.getMessage().getContentDisplay()
         );
